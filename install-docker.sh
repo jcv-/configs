012669d76371781363aef86a820907f5ba74d0d5
jcv- <jvanderpool99@gmail.com>
2025-02-06 15:19:23 -0500

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -12,4 +12,4 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 apt-get update
 
 # Install Docker:
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
